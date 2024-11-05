07cb2a9070ed609f425deb2ea22796cfc77ba12e
Nicolas-ruizg <166555822+Nicolas-ruizg@users.noreply.github.com>
2024-11-05 15:40:47 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
-
-print ("-------------------------------------------------------------------------------------------")
+print("-------------------------------------------------------------------------------------------")
 print("Bienvenido, este programa saca el promedio entre dos temperaturas hasta que la primera sea 0")
-print ("-------------------------------------------------------------------------------------------")
-temp1=(int(input("ingrese la primera temperatura: ")))
-temp2=(int(input("ingrese la segunda temperatura: ")))
+print("-------------------------------------------------------------------------------------------")
+
+
+temp1 = int(input("Ingrese la primera temperatura: "))
+temp2 = int(input("Ingrese la segunda temperatura: "))
+
+suma_temperaturas = 0
+contador = 0
 
 while True:
     if temp1 == 0:
-        print("La condicion se cumplio, la primera temperatura es 0 ")
+        print("La condición se cumplió, la primera temperatura es 0.")
         break
     else:
-        suma=temp1+temp2
-        promedio=suma/2
-        print("El promedio de las temperaturas es:",promedio)
+        
+        if 5 <= temp1 <= 15:
+            suma_temperaturas += temp1
+            contador += 1
+        
+       
+        if 5 <= temp2 <= 15:
+            suma_temperaturas += temp2
+            contador += 1
+
+        if contador > 0:
+            promedio = suma_temperaturas / contador
+            print(f"El promedio de las temperaturas válidas entre 5° y 15° es: {promedio:.2f}")
+        else:
+            print("No se ingresaron temperaturas válidas dentro del rango de 5° a 15°.")
+
         break
+
+
